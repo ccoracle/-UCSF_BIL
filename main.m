c81450d5b569bcedef3439d1a70fcc6e64a1d698
ccoracle <114243074+ccoracle@users.noreply.github.com>
2023-02-02 15:15:05 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,30 @@
+%% sbl with low resolution leadfield
+
+% ypost: sensor data after preprocessing
+% nd: # of directions
+% vcs = voxel covariance structure: 0 = scalar, 1 = diagonal, 2 = general
+% coup: covaraince update methods, 0=covex boounding, 1 = EM, 2 = Mackay;
+% prefer coup = 0
+% ncf: noise covavariance form, 0 = scalar, 1 = heter; prefer ncf = 1
+%
+
+nem = 100; %# of iterations for sbl
+rgamma = 1e-5;
+
+coup = 0;
+ncf = 1;
+[gamma,x,w,c]=SBL_cov(ypost,double(lf_low),nem,nd,vcs,1,coup,ncf);
+
+%% two ways to set the model data covariance first one:
+v = real(v);
+Ess = diag(v);
+Ryy_champ=lf_low*Ess*lf_low'+1e-5*max(eig(lf_low*Ess*lf_low'))*eye(size(lf_low*Ess*lf_low'));
+
+%% second
+Ryy_champ = c;
+
+%% beamformer+Champagne with low resolution leadfield
+[weight,~, po]= lcmv_par(double(LF3),Ryy_champ,0);
+weight = permute(weight,[1 3 2]);
+w = reshape(weight,size(weight,1),size(weight,2)*size(weight,3));
+x = w'*ypost; 
